5611b13d35a67d98d93fbfd76fb196223cc73042
hmabed <hmabed@gmail.com>
2025-09-01 15:27:28 +0200

    Delete src/lib directory

--- src/lib/pb.js
+++
@@ -1,9 +0,0 @@
-import PocketBase from "pocketbase";
-
-const PB_URL = "http://127.0.0.1:8090";
-
-// Une seule instance PocketBase côté client
-export const pb = new PocketBase(PB_URL); 
-export default pb;
-// pb.authStore persiste tout seul dans localStorage
-// donc l'utilisateur reste connecté même après refresh
